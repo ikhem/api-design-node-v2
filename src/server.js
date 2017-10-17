dea6565c72ea96d000ad4a1d1091fa63b08aabe5
Im Khem <im.khem@gmail.com>
2017-10-17 15:03:06 -0700

    lesson-10 in-progress

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import setupMiddware from './middleware'
-import { restRouter } from './api'
+import { restRouter, graphQLRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
+import { graphiqlExpress } from 'apollo-server-express'
 // Declare an app from express
 const app = express()
 
@@ -12,6 +13,9 @@ connect()
 
 app.use('/signin', signin)
 app.use('/api', protect, restRouter)
+app.use('/graphql', graphQLRouter)
+app.use('/docs', graphiqlExpress({ endpointURL: '/graphql'}))
+
 // catch all
 app.all('*', (req, res) => {
   res.json({ok: true})
